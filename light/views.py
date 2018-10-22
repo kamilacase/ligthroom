9b820d9483139ddf3409bf8f79850987c5fa297d
qrames <ramque@orange.fr>
2018-10-22 11:10:29 +0200

    (1) model --> admin : Light --> LightAdmin

--- light/views.py
+++ light/views.py
@@ -3,9 +3,4 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from django.views.generic import ListView
 # Create your views here.
-from .models import Light
-
-class LightListView(ListView):
-    model = Light
--- lightroom/urls.py
+++ lightroom/urls.py
@@ -16,9 +16,6 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from light.views import LightListView
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', LightListView.as_view()),
 ]
