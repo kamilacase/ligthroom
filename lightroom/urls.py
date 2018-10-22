07cde0198c5560385d1d2ac36b2099247924dc72
qrames <ramque@orange.fr>
2018-10-22 11:36:45 +0200

    static for node_modules

--- lightroom/urls.py
+++ lightroom/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 from light.views import LightListView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', LightListView.as_view(), name="light_list"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
