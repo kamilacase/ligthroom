58b30aa5df66958817f2dc058a29f122264f369d
qrames <ramque@orange.fr>
2018-10-22 11:11:43 +0200

    (2) url --> view --> template : LightListView.as_view --> class LightListView --> light_list.html

--- light/views.py
+++ light/views.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.views.generic import ListView
 # Create your views here.
+from .models import Light
+
+class LightListView(ListView):
+    model = Light
